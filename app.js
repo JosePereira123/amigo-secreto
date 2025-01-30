cdeaa815bfa67070306136ae97f22963aab614f1
JosePereira123 <josepereiras008@gmail.com>
2025-01-30 18:49:39 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo');
+    if (nome.value == '') {
+        alert ('Informe um nome valido!');
+        return;
+    }
+    if (nomes.includes == nome.value) {
+        alert ('Nome já esta na lista!');
+        return;
+    }
+
+nomes.push(nome.value);
+;
+
+    let lista = document.getElementById('listaAmigos');
+
+    lista.innerHTML = nome.value;
+nome.value = '' 
+return;   
+
+    
+}

0b402deff48d7e512e0b023b25428a9a610d3a73
JosePereira123 <josepereiras008@gmail.com>
2025-01-31 02:00:30 -0300

    Sorteio do nome

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ function adicionarAmigo() {
         alert ('Informe um nome valido!');
         return;
     }
-    if (nomes.includes == nome.value) {
+    if (nomes.includes (nome.value)) {
         alert ('Nome já esta na lista!');
         return;
     }
 
 nomes.push(nome.value);
-;
+
 
         let lista = document.getElementById('listaAmigos');
         lista.innerHTML = ''
@@ -21,7 +21,24 @@ nomes.push(nome.value);
     for (let i = 0; i < nomes.length; i++) {
         lista.innerHTML += '<li>' + nomes[i] + '</li>';
     }
-    
-    
+
+   amigo.value = '';
 }
+   function sortear() {
+        if (nomes.length < 2) {
+            alert ('Quantidade minima são 2 nomes');
+            return;
+        }
+            let indiceAleatorio = Math.floor(Math.random() * nomes.length); 
+            let resultadoFinal = document.getElementById("resultado");
+        resultadoFinal.innerHTML = nomes[indiceAleatorio];
+        }
+        
+   
+   
+
+   
+    
+    
+
 
